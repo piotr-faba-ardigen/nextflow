6149d1ca4141911393959f0de8882ba93f805d08
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2019-09-05 09:31:44 +0200

    wip

--- modules/nextflow/src/main/groovy/nextflow/executor/res/AcceleratorResource.groovy
+++ modules/nextflow/src/main/groovy/nextflow/executor/res/AcceleratorResource.groovy
@@ -20,24 +20,24 @@ import groovy.transform.CompileStatic
 import groovy.transform.EqualsAndHashCode
 
 /**
- * Models GPU resource request
+ * Models accelerator resource request
  * 
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
 @CompileStatic
 @EqualsAndHashCode
-class GpuResource {
+class AcceleratorResource {
 
     final Integer request
     final Integer limit
     final String type
     final String runtime
 
-    GpuResource( Number value ) {
+    AcceleratorResource(Number value ) {
         this(limit: value)
     }
 
-    GpuResource( Map res ) {
+    AcceleratorResource(Map res ) {
         if( res.limit!=null && res.request!=null ) {
             this.limit = res.limit as int
             this.request = res.request as int
